0e533d15e23042e77efc2cb6e3524e18f8533fa4
Hirxs <hirxsmc@gmail.com>
2025-06-05 01:19:04 -0600

    e

--- FF-for-mc-stable/BP/scripts/blocks/components/gravestone_vars.js
+++ FF-for-mc-stable/BP/scripts/blocks/components/gravestone_vars.js
@@ -1,1 +1,7 @@
- 
+export function registerGravestoneVars(registry) {
+  registry.registerCustomComponent("ff:gravestone_vars", {
+    onPlace: (onPlaceEvent => {
+      onPlaceEvent.block.setPermutation(onPlaceEvent.block.permutation.withState("ff:stocking_vars", Math.round(Math.random() * 3)))
+    })
+  });
+} 
